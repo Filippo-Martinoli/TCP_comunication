764870979054980d0fbb020d5e40200dae018d05
Filippo-Martinoli <filippomartinoli07@gmail.com>
2026-02-09 21:17:26 +0100

    MainClient.java

--- src/MainClient.java
+++ src/MainClient.java
@@ -1,5 +1,17 @@
-public class  MainClient {
-    public static void main(String[] args){
-Client client = new Client();
+public class MainClient {
+    public static void main(String[] args) {
+        Client client = new Client();
+        int esito = client.connetti("localhost", 12345);
+
+        if (esito == 0) {
+
+            client.scrivi("Richiesta dal client");
+            client.leggi();
+            client.chiudi();
+
+        } else {
+
+            System.out.println("Server non disponibile");
+        }
     }
 }
