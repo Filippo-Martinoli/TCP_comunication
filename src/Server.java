32f31aa2f3bbfcf7a38b14ca40ca2ad7c9b72121
Filippo-Martinoli <filippomartinoli07@gmail.com>
2026-02-09 21:23:24 +0100

    Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,36 +1,98 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+
     private ServerSocket serverSocket;
     private Socket socket;
     private int porta;
+    private PrintWriter out;
+    private BufferedReader in;
 
-    public Server(int porta) throws IOException {
-        this.porta = this.porta;
-        this.serverSocket = serverSocket;
-        serverSocket = new ServerSocket(porta);
-
+    public ServerSocket getServerSocket() {
+        return serverSocket;
     }
-    public Socket attendi(){
-        try {
-            socket = serverSocket.accept();
-        } catch (IOException e) {
-            // il server non riesci ad accettare la connessione con il client
-        }
+
+    public Server setServerSocket(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+        return this;
+    }
+
+    public Socket getClientSocket() {
         return socket;
     }
-    public void scrivi(){
 
+    public Server setClientSocket(Socket clientSocket) {
+        this.socket = clientSocket;
+        return this;
     }
-    public void leggi(){
 
+    public int getPorta() {
+        return porta;
     }
-    public void chiudi(){
 
+    public Server(int porta) throws IOException {
+        this.porta = porta;
+        serverSocket = new ServerSocket(porta);
     }
-    public void termina(){
 
+    public Socket attendi() {
+
+        try {
+            socket = serverSocket.accept();
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream())
+            );
+
+        } catch (IOException e) {
+            System.out.println("Errore di accettazione del client");
+        }
+
+        return socket;
+    }
+
+    public void scrivi(String msg) {
+        if (out != null) {
+            out.println(msg);
+        }
+    }
+
+    public String leggi() {
+
+        try {
+            return in.readLine();
+
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public void chiudi() {
+
+        try {
+            if (socket != null) {
+                socket.close();
+            }
+
+        } catch (IOException e) {
+        }
+    }
+
+    public void termina() {
+
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+
+            System.out.println("il Server è terminato");
+
+        } catch (IOException e) {
+        }
     }
 }
