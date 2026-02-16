a4de645d0bb9f04fdbcc995990d8ba18441e4780
Filippo-Martinoli <filippomartinoli07@gmail.com>
2026-02-16 22:04:45 +0100

    MainServer.java

--- src/MainServer.java
+++ src/MainServer.java
@@ -1,30 +1,21 @@
-import java.io.IOException;
-import java.net.BindException;
-import java.net.Socket;
-
 public class MainServer {
+
     public static void main(String[] args) {
-        int porta = 12345;
+        Server server = new Server(12345);
 
-        try {
-            Server server = new Server(porta);
-            System.out.println("Il server è in ascolto");
-            Socket socket = server.attendi();
-            System.out.println("Il server è connesso");
-            String richiesta = server.leggi();
-            System.out.println("Richiesta ricevuta: " + richiesta);
-            server.scrivi("Risposta del server ricevuta ");
+        while (true) {
+            server.attendi();
+            String msg = "";
+
+            while (!msg.equals("fine")) {
+                msg = server.ricevi();
+                if (msg == null) break;
+                System.out.println("Ricevuto: " + msg);
+                if (!msg.equals("fine")) {
+                    server.invia("Ricevuto: " + msg);
+                }
+            }
             server.chiudi();
-            System.out.println("Connessione chiusa");
-            server.termina();
-
-        } catch (BindException e) {
-            System.out.println("La porta è occupata");
-
-        } catch (IOException e) {
-            System.out.println("Errore di avvio del server");
-            e.printStackTrace();
-            System.exit(1);
         }
     }
 }
