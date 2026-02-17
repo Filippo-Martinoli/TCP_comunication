e89dcc5eca7fd4050e067bbb33cbb84ae93e2220
Filippo-Martinoli <filippomartinoli07@gmail.com>
2026-02-17 15:01:36 +0100

    MainClient.java

--- src/MainClient.java
+++ src/MainClient.java
@@ -1,5 +1,29 @@
-public class  MainClient {
-    public static void main(String[] args){
-Client client = new Client();
+import java.util.Scanner;
+
+public class MainClient {
+
+    public static void main(String[] args) {
+
+        Client client = new Client();
+        client.connetti("localhost", 3241);
+
+        Scanner tastiera = new Scanner(System.in);
+
+        String msg = "";
+
+        while (!msg.equals("fine")) {
+
+            System.out.print("Inserisci messaggio: ");
+            msg = tastiera.nextLine();
+
+            client.invia(msg);
+
+            if (!msg.equals("fine")) {
+                String risposta = client.ricevi();
+                System.out.println("Server " + risposta);
+            }
+        }
+
+        client.chiudi();
     }
 }
