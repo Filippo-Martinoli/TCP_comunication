78dc56041fd078190f4e66b161370383459cd737
Gaia Mincigrcci <gaia.mincigrucci@gmail.com>
2026-03-03 14:17:23 +0100

    codice aggiornato

--- src/MainClient.java
+++ src/MainClient.java
@@ -1,5 +1,29 @@
-public class  MainClient {
-    public static void main(String[] args){
-Client client = new Client();
+import java.util.Scanner;
+
+public class MainClient {
+
+    public static void main(String[] args) {
+
+        Client client = new Client();
+        client.connetti("localhost", 12345);
+
+        Scanner tastiera = new Scanner(System.in);
+
+        String msg = "";
+
+        while (!msg.equals("fine")) {
+
+            System.out.print("Inserisci messaggio: ");
+            msg = tastiera.nextLine();
+
+            client.scrivi(msg);
+
+            if (!msg.equals("fine")) {
+                String risposta = client.leggi();
+                System.out.println("Server " + risposta);
+            }
+        }
+
+        client.chiudi();
     }
 }
